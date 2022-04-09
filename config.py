4e7942340a3ec0c947ff31cbdad3995fcbfa06e4
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-04-09 11:56:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ import json
 with open("config.json","r") as f:
     config = json.loads(f.read())
 
-TOKEN=config.get("token")
+TOKEN=config.get("5146219052:AAE5NZAkuPOKIVYl9AsSg4oWPdDEww_O8JU")
 WORKERS=config.get("workers", 32)
 ADMIN_LIST = config.get("admin_list", None)
 OPEN_LOBBY = config.get("open_lobby", True)
@@ -32,4 +32,4 @@ DEFAULT_GAMEMODE = config.get("default_gamemode", "fast")
 WAITING_TIME = config.get("waiting_time", 120)
 TIME_REMOVAL_AFTER_SKIP = config.get("time_removal_after_skip", 20)
 MIN_FAST_TURN_TIME = config.get("min_fast_turn_time", 15)
-MIN_PLAYERS = config.get("min_players", 2)
+MIN_PLAYERS = config.get("min_players", 4)
