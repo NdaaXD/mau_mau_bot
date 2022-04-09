54fc7d7d6e0880246d25dd4737543006b3352ee3
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-04-09 11:57:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ import json
 with open("config.json","r") as f:
     config = json.loads(f.read())
 
-TOKEN=config.get("5146219052:AAE5NZAkuPOKIVYl9AsSg4oWPdDEww_O8JU")
+TOKEN=config.get("token")
 WORKERS=config.get("workers", 32)
 ADMIN_LIST = config.get("admin_list", None)
 OPEN_LOBBY = config.get("open_lobby", True)
